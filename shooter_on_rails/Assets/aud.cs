86a5425708a40cc913afb5887d953046f1d98309
jsikkenga <55822197+jsikkenga@users.noreply.github.com>
2020-06-25 21:51:31 -0700

    creating rails

---
+++ shooter_on_rails/Assets/aud.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class aud : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    
+    void Start()
+    {
+        Invoke("LoadFirstScene", 3f);
+
+        
+        
+    }
+    void LoadFirstScene()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(1);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
